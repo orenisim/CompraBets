122695520ed8b41f2e3a3e75b5983401445c8833
orenisim <110765627+orenisim@users.noreply.github.com>
2022-10-18 04:48:01 -0700

    Add files via upload

--- JS/joinLeague.js
+++ JS/joinLeague.js
@@ -1,10 +1,28 @@
+//indicators to the relebent html for the add new league option
 const createNewLeagueButton = document.querySelector("#createNewLeagueButton");
 const newLeagueForm = document.querySelector(".newLeagueForm");
+
+//idicators to the relevent html for the search league method
+const searchLeague = document.querySelector("#searchLeagueInput");
+const listLeagues = document.querySelector("#listOfLeagues");
 
 // open the new league creator after clicking the "Create New League" button
 createNewLeagueButton.addEventListener("click", () => {
   newLeagueForm.classList.toggle("d-none");
 });
 
-const array = [122345, 634221, 473890, 330981 ];
+//filtering the correct league during the search
+const filterLeaguea = (term) => {
+  Array.from(listLeagues.children)
+    .filter((league) => !league.textContent.includes(term))
+    .forEach((league) => league.classList.add("filterd"));
 
+  Array.from(listLeagues.children)
+    .filter((league) => league.textContent.includes(term))
+    .forEach((league) => league.classList.remove("filterd"));
+};
+
+searchLeague.addEventListener("keyup", () => {
+  const term = searchLeague.value.trim();
+  filterLeaguea(term);
+});
