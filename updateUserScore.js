e7b2af076c69df923f80a13fc547e5333acbd5e7
orenisim <110765627+orenisim@users.noreply.github.com>
2022-11-12 23:22:58 +0200

    Add files via upload

---
+++ updateUserScore.js
@@ -0,0 +1,96 @@
+import { db, getArrayOfIDfromUsers, getCurrentScore } from "./usersFirebase.js";
+import { getArrayOfBets } from "./betsFirebase.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc, //init
+  addDoc, //add
+  query,
+  where, //QUERIES
+  orderBy,
+  serverTimestamp,
+  updateDoc,
+  getCountFromServer,
+  doc,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+
+const usersColRef = collection(db, "users");
+const matchesColRef = collection(db, "matches");
+
+//getting the game score by ID:
+const getGameScoreByID = async (apiID) => {
+  const q = query(matchesColRef, where("api_ID", "==", apiID));
+  const data = await getDocs(q);
+  let matchObject;
+  data.forEach((doc) => (matchObject = { ...doc.data(), id: doc.id }));
+  return matchObject.finalScore;
+};
+
+//getting the reall winning team by ID
+const getGameWinnerByID = async (apiID) => {
+  const q = query(matchesColRef, where("api_ID", "==", apiID));
+  const data = await getDocs(q);
+  let matchObject;
+  data.forEach((doc) => (matchObject = { ...doc.data(), id: doc.id }));
+  return matchObject.winner;
+};
+
+//getting the user game bet by id:
+const getBetScoreByID = async (apiID, userID) => {
+  const betsColRef = collection(db, `users/${userID}/Bets`);
+  const q = query(betsColRef, where("api_ID", "==", apiID));
+  const data = await getDocs(q);
+  let matchObject;
+  data.forEach((doc) => (matchObject = { ...doc.data(), id: doc.id }));
+  return matchObject.finalScore;
+};
+
+//getting the user winnig team by id:
+const getBetWinnerByID = async (apiID, userID) => {
+  const betsColRef = collection(db, `users/${userID}/Bets`);
+  const q = query(betsColRef, where("api_ID", "==", apiID));
+  const data = await getDocs(q);
+  let matchObject;
+  data.forEach((doc) => (matchObject = { ...doc.data(), id: doc.id }));
+  return matchObject.winner;
+};
+
+//checking the score and return the amount of points that the user earned.
+export const checkingScore = async (apiID, userID) => {
+  const reallScore = await getGameScoreByID(apiID);
+  const betScore = await getBetScoreByID(apiID, userID);
+  const reallWinner = await getGameWinnerByID(apiID);
+  const betWiner = await getBetWinnerByID(apiID, userID);
+
+  //super score = 10
+  if (reallScore == betScore) return 10;
+  //regular score = 5
+  else if (reallWinner == betWiner) return 5;
+  //false guess = 0
+  else return 0;
+};
+
+//updating the score field in the user document
+export const updateScore = async () => {
+  const arrayOfUsersID = await getArrayOfIDfromUsers();
+  arrayOfUsersID.forEach(async (userID) => {
+    let betEarnPoints = await getCurrentScore(userID);
+    console.log(betEarnPoints);
+    const userColRef = await doc(db, "users", userID);
+    const arrayOfUserBets = await getArrayOfBets(userID);
+    arrayOfUserBets.forEach(async (bet) => {
+      const api_ID = bet.api_ID;
+      //needs to check only in case to ignore of empty documents --> inside the if is only tests right now...
+      betEarnPoints += await checkingScore(api_ID, userID);
+    });
+    console.log(`user ${userID} earned: ${betEarnPoints}`);
+    // await updateDoc(userColRef, {
+    //   score: betEarnPoints,
+    // });
+    return betEarnPoints;
+  });
+};
+
+updateScore();
